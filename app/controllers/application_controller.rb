50addd8f268384931f0f7406c6594b8971f140d2
Cole Rau <colevrau@gmail.com>
2020-05-20 18:31:29 -0700

    realized a user needs to have many teams

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,9 @@ class ApplicationController < Sinatra::Base
     get '/' do
       erb :"application/index"
     end
+
+    get '/start' do 
+        erb :"/application/trivia_sheet"
+    end
 end
   
--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -11,16 +11,66 @@ class TeamsController < ApplicationController
     post '/teams' do 
         binding.pry
         # t1 stands for team one
-        @t1 = Team.create(name: params[:t1n])
-        @t2 = Team.create(name: params[:t2n])
-        @t3 = Team.create(name: params[:t3n])
-        @t4 = Team.create(name: params[:t4n])
-        @t5 = Team.create(name: params[:t5n])
-        @t6 = Team.create(name: params[:t6n])
-        @t7 = Team.create(name: params[:t7n])
-        @t8 = Team.create(name: params[:t8n])
-        @t9 = Team.create(name: params[:t9n])
-        @t10 = Team.create(name: params[:t10n])
+        if !(Team.find_by(name: params[:t1n]))
+            @t1 = Team.create(name: params[:t1n])
+        else 
+            redirect '/teams/new'
+        end 
+
+        if !(Team.find_by(name: params[:t2n]))
+            @t2 = Team.create(name: params[:t2n])
+        else 
+            redirect '/teams/new'
+        end 
+
+        if !(Team.find_by(name: params[:t3n]))
+            @t3 = Team.create(name: params[:t3n])
+        else 
+            redirect '/teams/new'
+        end 
+
+        if !(Team.find_by(name: params[:t4n]))
+            @t4 = Team.create(name: params[:t4n])
+        else 
+            redirect '/teams/new'
+        end 
+
+        if !(Team.find_by(name: params[:t5n]))
+            @t5 = Team.create(name: params[:t5n])
+        else 
+            redirect '/teams/new'
+        end 
+
+        if !(Team.find_by(name: params[:t6n]))
+            @t6 = Team.create(name: params[:t6n])
+        else 
+            redirect '/teams/new'
+        end 
+
+        if !(Team.find_by(name: params[:t7n]))
+            @t7 = Team.create(name: params[:t7n])
+        else 
+            redirect '/teams/new'
+        end 
+
+        if !(Team.find_by(name: params[:t8n]))
+            @t8 = Team.create(name: params[:t8n])
+        else 
+            redirect '/teams/new'
+        end 
+
+        if !(Team.find_by(name: params[:t9n]))
+            @t9 = Team.create(name: params[:t9n])
+        else 
+            redirect '/teams/new'
+        end 
+
+        if !(Team.find_by(name: params[:t10n]))
+            @t10 = Team.create(name: params[:t10n])
+        else 
+            redirect '/teams/new'
+        end 
+
         erb :'/teams/show'
     end
 
