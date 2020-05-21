cf3516cfea9d85882b95082f7ef888a1a54233c2
Cole Rau <colevrau@gmail.com>
2020-05-20 18:06:03 -0700

    user can create a new team

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,6 +1,7 @@
 class TeamsController < ApplicationController
     get '/teams' do 
         @teams = Team.all
+        binding.pry
     end
 
     get '/teams/new' do 
@@ -9,7 +10,17 @@ class TeamsController < ApplicationController
 
     post '/teams' do 
         binding.pry
-        @team = params[:team_name]
+        # t1 stands for team one
+        @t1 = Team.create(name: params[:t1n])
+        @t2 = Team.create(name: params[:t2n])
+        @t3 = Team.create(name: params[:t3n])
+        @t4 = Team.create(name: params[:t4n])
+        @t5 = Team.create(name: params[:t5n])
+        @t6 = Team.create(name: params[:t6n])
+        @t7 = Team.create(name: params[:t7n])
+        @t8 = Team.create(name: params[:t8n])
+        @t9 = Team.create(name: params[:t9n])
+        @t10 = Team.create(name: params[:t10n])
         erb :'/teams/show'
     end
 
